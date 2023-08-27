bc35bf0caa37cbf1b2065d1254a76da1ffa90c97
taka392 <gongguyiji@gmail.com>
2023-08-27 15:16:34 +0900

    #3 import_sorter_finish

---
+++ lib/custom_text.dart
@@ -0,0 +1,11 @@
+// Flutter imports:
+import 'package:flutter/cupertino.dart';
+
+class CustomText extends StatelessWidget {
+  const CustomText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("aa");
+  }
+}
