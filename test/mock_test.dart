71fc1ebb6ddbb55f951b9948239e306f68bb479b
taka392 <gongguyiji@gmail.com>
2023-08-29 11:49:46 +0900

    #11 add_full_name_test

--- test/mock_test.dart
+++ test/mock_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:search_repo/domain/types/repo_model.dart';
+import 'package:search_repo/domain/types/item_model.dart';
 import 'mock_data.dart';
 
 void main() {
@@ -8,5 +9,6 @@ void main() {
     final data = json.decode(MockData.jsonMock);
     final RepoModel result = RepoModel.fromJson(data);
     expect(result.totalCount, 573491);
+    expect(result.items[0].fullName, "flutter/flutter");
   });
 }
