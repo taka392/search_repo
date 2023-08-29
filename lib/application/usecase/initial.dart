4d385838a925b3d1112577dab4793361cc94ee51
taka392 <gongguyiji@gmail.com>
2023-08-29 16:27:53 +0900

    #15 create initial

--- lib/application/usecase/initial.dart
+++ lib/application/usecase/initial.dart
@@ -1,5 +1,5 @@
-import 'package:search_repo/application/di/post_repo_imt.dart';
 import 'package:search_repo/application/state/repo.dart';
+import 'package:search_repo/domain/types/repo_abstract.dart';
 import 'package:search_repo/domain/types/repo_model.dart';
 
 /// アプリの初期準備をする
@@ -8,14 +8,14 @@ class InitAppUsecase {
     required this.postRepositoryImpl,
     required this.repoProviderNotifier,
   });
-  final PostRepositoryImpl postRepositoryImpl;
+  final PostRepository postRepositoryImpl;
   final RepoNotifier repoProviderNotifier;
 
   /// 一連の流れをまとめて実施する
   Future<void> fetch() async {
     //初期値を取得
-    final repo =postRepositoryImpl.getPosts();
+    final repo = await postRepositoryImpl.getPosts();
     //Riverpodに保存
-    repoProviderNotifier.save(repo as RepoModel);
+    repoProviderNotifier.save(repo);
   }
 }
--- lib/presentation/pages/list_page.dart
+++ lib/presentation/pages/list_page.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
-class ListPage extends StatelessWidget {
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_repo/application/di/useusecase.dart';
+import 'package:search_repo/application/state/repo.dart';
+
+// 例: StateProviderを使用した状態管理
+final counterProvider = StateProvider<int>((ref) => 0);
+
+class ListPage extends HookConsumerWidget {
   const ListPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Text("ss");
+  Widget build(BuildContext context, WidgetRef ref) {
+    final repo = ref.watch(repoNotifierProvider); // StateProviderから状態を取得
+
+    useEffect(() {
+      // スプラッシュ画面がないのでここで初期化
+      final usecase = ref.read(initAppProvider);
+      usecase.fetch();
+      return null;
+    }, const []);
+
+    final s3Text = repo.when(
+      loading: () => const Text('準備中...'),
+      error: (e, s) => Text('エラー $e'),
+      data: (d) => Text(d.toString()),
+    );
+
+
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        s3Text,
+      ],
+    );
   }
 }
+
+
