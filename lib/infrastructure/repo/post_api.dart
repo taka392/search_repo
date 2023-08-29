7f545fb523aa010d039a6f0b1774c3f60a1cf05f
taka392 <gongguyiji@gmail.com>
2023-08-29 12:45:28 +0900

    #13 api_post_data_source

--- lib/infrastructure/repo/post_api.dart
+++ lib/infrastructure/repo/post_api.dart
@@ -1,12 +1,22 @@
 import 'package:search_repo/domain/types/repo_abstract.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:search_repo/domain/types/repo_model.dart';
 // データソースの実装
 class ApiPostDataSource implements PostRepository {
   final http.Client httpClient;
-
   ApiPostDataSource(this.httpClient);
 
   @override
-  Future<List<PostEntity>> getPosts() async {
-    // データをAPIから取得し、エンティティのリストに変換する処理
+  Future<List<RepoModel>> getPosts() async {
+    final response = await http.get(Uri.parse(
+        'https://api.github.com/search/repositories?q=Flutter&per_page=20'));
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final RepoModel aa = RepoModel.fromJson(data);
+      return aa;
+    } else {
+      throw Exception('Invalid JSON response structure');
+    }
   }
 }
