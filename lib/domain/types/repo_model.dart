50f3043e5ac9825dee1b2de6f4d39ae4720a0527
taka392 <gongguyiji@gmail.com>
2023-08-29 11:09:04 +0900

    #11 create_owner_model

--- lib/domain/types/repo_model.dart
+++ lib/domain/types/repo_model.dart
@@ -1,17 +1,17 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:search_repo/domain/types/items_data_model.dart';
+import 'package:search_repo/domain/types/item_model.dart';
 part 'repo_model.freezed.dart';
 
 @freezed
 class RepoModel with _$RepoModel {
   const factory RepoDataModel({
     required int totalCount,
-    required List<Item> items,
+    required List<ItemModel> items,
   }) = _RepoModel;
 
   factory RepoModel.fromJson(Map<String, dynamic> json) {
     List<dynamic> itemsJson = json['items'];
-    List<Item> items = itemsJson.map((json) => Item.fromJson(json)).toList();
+    List<ItemModel> items = itemsJson.map((json) => ItemModel.fromJson(json)).toList();
     return RepoModel(
       totalCount: json['total_count'] ?? 0,
       items: items,
