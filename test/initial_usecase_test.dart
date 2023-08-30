0d8c448c0fe8b29c865e3f5d3e86f9abd0784e2f
taka392 <gongguyiji@gmail.com>
2023-08-30 14:17:26 +0900

    #17 delete_ map string dynamic

--- test/initial_usecase_test.dart
+++ test/initial_usecase_test.dart
@@ -1,27 +1,28 @@
-import 'package:flutter/cupertino.dart';
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/testing.dart';
 import 'package:mockito/mockito.dart';
-import 'package:search_repo/application/di/repo_di.dart';
-import 'package:search_repo/application/state/repo.dart';
-import 'package:search_repo/application/usecase/initial.dart';
-import 'package:search_repo/domain/types/repo_abstract.dart';
+
 import 'package:search_repo/domain/types/repo_model.dart';
 import 'package:search_repo/presentation/pages/list_page.dart';
-
-import 'initial_mock.dart';
 import 'mock_data.dart';
 
 
-class MockYourRepository extends Mock implements PostRepository {}
+class MockClient extends Mock implements http.Client {}
 void main() {
   group('GetRepoUseCase', () {
     testWidgets('listが表示されるかテストします。', (WidgetTester tester) async {
 
-      final mockRepository = MockYourRepository();
-      const data = MockData.jsonMock;
-      final repo = RepoModel.fromJson(data as Map<String, dynamic>);
+      final client = MockClient();
+      http.Response response = http.Response(MockData.jsonMock, 200);
+
+      when(client.get(any)).thenAnswer((_) async => response);
+      final repo = RepoModel.fromJson(data);
 
       // モックのデータを定義
       when(mockRepository.getPosts()).thenAnswer((_) async {
