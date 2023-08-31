46dbbe320b6d52f45e223ead8330d86a08e9e872
taka392 <gongguyiji@gmail.com>
2023-08-31 20:41:31 +0900

    #22 add_animation_compornent

---
+++ lib/presentation/widget/custom_animation.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:lottie/lottie.dart';
+class CustomError extends ConsumerWidget {
+  final String imageUrl;
+  final String text;
+
+  const CustomError({Key? key, required this.imageUrl, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return RefreshIndicator(
+      color: Colors.grey,
+      onRefresh: () async {},
+      child: ListView(
+        children: [
+          const SizedBox(height: 80),
+          Lottie.asset(imageUrl), // Load a Lottie file from the provided URL
+          Text(text,textAlign: TextAlign.center,),
+        ],
+      ),
+    );
+  }
+}
+
