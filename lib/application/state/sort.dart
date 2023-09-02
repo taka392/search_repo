72d671cff99facad6ed4688a1eb466919065ffc2
taka392 <gongguyiji@gmail.com>
2023-09-02 11:53:32 +0900

    #24 add_sort_state

---
+++ lib/application/state/sort.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'sort.g.dart';
+@riverpod
+class SortNotifier extends _$SortNotifier {
+  @override
+  String build() {
+    return '';
+  }
+}
+
+
