498837aeeefa42e711c9c78d77d28c59d134e7c4
taka392 <gongguyiji@gmail.com>
2023-09-02 11:52:50 +0900

    #24 add_page_state

---
+++ lib/application/state/page.dart
@@ -0,0 +1,10 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'page.g.dart';
+
+@Riverpod(keepAlive: true)
+class PageNotifier extends _$PageNotifier {
+  @override
+  int build(){
+    return  1;
+  }
+}
