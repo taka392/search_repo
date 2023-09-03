2a94c843b80485b8b518dd0a38c8846fccce0b82
taka392 <gongguyiji@gmail.com>
2023-09-03 13:06:37 +0900

    #26 add_search_usecase

--- lib/application/usecase/search_usecase.dart
+++ lib/application/usecase/search_usecase.dart
@@ -7,24 +7,25 @@ import 'package:search_repo/infrastructure/repo/repo.dart';
 
 
 /// アプリの初期準備をする
-class AddUsecase {
-  AddUsecase({
+class SearchUsecase {
+  SearchUsecase({
     required this.repo,
-    required this.pageNotifier,
+    required this.search,
+    required this.searchNotifier,
     required this.repoNotifier,
-    required this.controller,
     //以前のページ
   });
   final Repo repo;
-  final PageNotifier pageNotifier;
+  final String search;
+  final PageNotifier searchNotifier;
   final RepoNotifier repoNotifier;
-  final ScrollController? controller;
 
 
   /// 一連の流れをまとめて実施する
   Future<void> search() async {
-    //検索結果を受け取る
+    //検索結果を保存
     //検索結果を元にRepoを取得
+    //検索結果を保存する
     //Stateに保存する。
   }
 }
