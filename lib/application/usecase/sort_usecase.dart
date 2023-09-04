3ef46bfeaef3afdbc8df66bfbcbf2f3fc37104b0
taka392 <gongguyiji@gmail.com>
2023-09-04 10:15:40 +0900

    #30 add_value_to_usecase

--- lib/application/usecase/sort_usecase.dart
+++ lib/application/usecase/sort_usecase.dart
@@ -12,18 +12,23 @@ class SortUsecase {
     required this.repo,
     required this.sortNotifier,
     required this.repoNotifier,
+    required this.value,
     //以前のページ
   });
   final Repo repo;
   final SortNotifier sortNotifier;
   final RepoNotifier repoNotifier;
+  final String value;
 
 
   /// 一連の流れをまとめて実施する
   Future<void> search() async {
     //選択されたテキストで新しいリポジトリを取得
+    RepoModel data = await repo.getRepo();
     //リポジトリのState値を更新
+    repoNotifier.save(data);
     //SortのState値を更新
+    sortNotifier.save(value);
 
     //repo
     RepoModel data = await repo.getRepo();
