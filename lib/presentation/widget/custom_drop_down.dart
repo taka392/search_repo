67797606a97c23982f45dba67e39715057d41cb6
taka392 <gongguyiji@gmail.com>
2023-09-04 09:54:07 +0900

    #30 add_dropdown_widget

---
+++ lib/presentation/widget/custom_drop_down.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_repo/domain/types/sort_model.dart';
+
+class CustomDropdown extends ConsumerWidget {
+  const CustomDropdown({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const items = [
+      DropdownMenuItem(
+        value: Sort.stars,
+        child: Text('stars'),
+      ),
+      DropdownMenuItem(
+        value: Sort.forks,
+        child: Text('forks'),
+      ),
+      DropdownMenuItem(
+        value: Sort.helpWantedIssues,
+        child: Text('help-wanted-issues'),
+      ),
+      DropdownMenuItem(
+        value: Sort.updated,
+        child: Text('updated'),
+      ),
+    ];
+
+    return DropdownButton(
+      value: Sort.updated,
+      items: items,
+      onChanged: (selectedValue) {
+        // 選択が変更されたときに何かを行う
+        // selectedValueはDropdownValue型として利用可能
+      },
+      focusColor: Colors.transparent,
+    );
+  }
+}
