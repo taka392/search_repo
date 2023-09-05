9d73f18299dc4ffb1b5032aff0d95885919d5fec
taka392 <gongguyiji@gmail.com>
2023-09-05 15:13:33 +0900

    #32 scroll_controller_add_to_ci

--- test/presentation/async_value_test.dart
+++ test/presentation/async_value_test.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:search_repo/domain/types/repo_model.dart';
