2cd24d8b526b6b5145e05772eed9f0ddbe6a5a30
taka392 <gongguyiji@gmail.com>
2023-09-06 08:04:32 +0900

    add_text_style_to_dropdown

--- lib/presentation/widget/custom_drop_down.dart
+++ lib/presentation/widget/custom_drop_down.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -6,11 +5,13 @@ import 'package:search_repo/application/di/usecase_di.dart';
 import 'package:search_repo/application/state/l10n/applocalizatons_provider.dart';
 import 'package:search_repo/application/state/sort/sort.dart';
 import 'package:search_repo/domain/types/sort_enum.dart';
+import 'package:search_repo/presentation/theme/fonts.dart';
 import 'package:tuple/tuple.dart';
 
 class CustomDropdown extends ConsumerWidget {
   final ScrollController scrollController;
-  const CustomDropdown({super.key,required this.scrollController});
+
+  const CustomDropdown({super.key, required this.scrollController});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -19,32 +20,45 @@ class CustomDropdown extends ConsumerWidget {
     final items = [
       DropdownMenuItem(
         value: Sort.stars,
-        child: Text(locate.stars),
+        child: Text(
+          locate.stars,
+          style: CustomText.titleM,
+        ),
       ),
       DropdownMenuItem(
         value: Sort.forks,
-        child: Text(locate.forks),
+        child: Text(
+          locate.forks,
+          style: CustomText.titleM,
+        ),
       ),
       DropdownMenuItem(
         value: Sort.helpWantedIssues,
-        child: Text(locate.help_wanted_issues),
+        child: Text(
+          locate.help_wanted_issues,
+          style: CustomText.titleM,
+        ),
       ),
       DropdownMenuItem(
         value: Sort.updated,
-        child: Text(locate.updated),
-      ),
+        child: Text(
+          locate.updated,
+          style: CustomText.titleM,
+        ),
+      )
     ];
 
     return DropdownButton<Sort>(
-        value: sort,
-        items: items,
-        onChanged: (value) {
-          final usecase = ref.read(sortProvider(Tuple2(value!, scrollController)));
-          usecase.sort();
-        },
-          isDense: false,
-          borderRadius: BorderRadius.circular(20.0),
-        underline: const SizedBox(),
+      value: sort,
+      items: items,
+      onChanged: (value) {
+        final usecase =
+            ref.read(sortProvider(Tuple2(value!, scrollController)));
+        usecase.sort();
+      },
+      isDense: false,
+      borderRadius: BorderRadius.circular(20.0),
+      underline: const SizedBox(),
     );
   }
 }
